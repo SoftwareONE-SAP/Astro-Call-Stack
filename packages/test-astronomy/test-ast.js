b2c2dc9f5b84a95c3f1e4ebc83529c3fbe63cbc1
Jake Lacey <jakelacey2012@gmail.com>
2016-10-12 15:38:48 +0100

    Removing code which doesn't corrilate to the call stack error

--- packages/test-astronomy/test-ast.js
+++ packages/test-astronomy/test-ast.js
@@ -1,4 +1,11 @@
 /**
+ * Activity   - Commented out no callstack error.
+ * Resolve
+ * mediaLink
+ * AS.Object - inherits resolvable
+ */
+
+/**
  * Declare the AS exportable symbol
  * @type {Object}
  */
@@ -27,46 +34,18 @@ AS.Resolvable = Astro.Class.create({
 	}
 });
 
-AS.MediaLink = Astro.Class.create({
-	name: 'AS.MediaLink',
-	typeField: 'objectType',
-	fields: {
-		url: {
-			type: String,
-		},
-		duration: {
-			type: Number,
-			optional: true
-		},
-		height: {
-			type: Number,
-			optional: true
-		},
-		width: {
-			type: Number,
-			optional: true
-		}
-	}
-});
-
+// AS.Object = Astro.Class.create({
 AS.Object = AS.Resolvable.inherit({
 	name: 'AS.Object',
 	typeField: 'objectType',
-	behaviors: {
-		timestamp: {
-			hasCreatedField: 	true,
-			hasUpdatedField: 	true,
-			createdFieldName: 	'published',
-			updatedFieldName: 	'updated'
-		}
-	}
 });
 
 AS.Object.extend({
 	fields: {
-		objectType: {
-			type: String,
-		},
+    /**
+     * @NOTE
+     * Commenting out these lines, will not cause a call stack error
+     */
 		attachments: {
 			type: [AS.Object],
 			optional: true
@@ -75,152 +54,20 @@ AS.Object.extend({
 			type: AS.Object,
 			optional: true
 		},
-		content: {
-			type: String,
-			optional: true
-		},
-		displayName: {
-			type: String,
-			optional: true
-		},
-		downstreamDuplicates: {
-			type: [String],
-			optional: true
-		},
-		upstreamDuplicates: {
-			type: [String],
-			optional: true
-		},
-		id: {
-			type: String,
-		},
-		image: {
-			type: AS.MediaLink,
-			optional: true
-		},
-		published: {
-			type: Date,
-			immutable: true,
-			default: function() { return new Date(); },
-		},
-		summary: {
-			type: String,
-			optional: true
-		},
-		updated: {
-			type: Date,
-			default: function() { return new Date(); },
-		},
-		url: {
-			type: String,
-			optional: true
-		},
-		parentType: {
-			type: String,
-			transient: true,
-			immutable: true,
-			default: null,
-		},
 	},
-	events: {
-		afterInit(e) {
-			e.currentTarget.parentType = e.currentTarget.constructor.getParent().getName();
-		}
-	}
 });
 
+/**
+ * @NOTE
+ * Commenting the activity class, will not cause a call stack error.
+ */
 AS.Activity = Astro.Class.create({
-	name: 'AS.Activity',
+	name:      'AS.Activity',
 	typeField: 'objectType',
 	collection: new Meteor.Collection('as.activities'),
 	fields: {
-		actor: {
+		fieldA: {
 			type: AS.Object,
-		},
-		object: {
-			type: AS.Object,
-		},
-		verb: {
-			type: String,
-			index: 1,
-		},
-		published: {
-			type: Date,
-			immutable: true,
-			default: function() { return new Date(); },
-		},
-		content: {
-			type: String,
-			optional: true
-		},
-		generator: {
-			type: AS.Object,
-			optional: true
-		},
-		icon: {
-			type: AS.MediaLink,
-			optional: true
-		},
-		id: {
-			type: String,
-			transient: true,
-			immutable: true,
-		},
-		provider: {
-			type: AS.Object,
-			optional: true
-		},
-		target: {
-			type: AS.Object,
-			optional: true
-		},
-		title: {
-			type: String,
-			optional: true
-		},
-		updated: {
-			type: Date,
-			default: function() { return new Date(); },
-			index: 1,
-		},
-		url: {
-			type: String,
-			optional: true
-		}
-	},
-	behaviors: {
-		timestamp: {
-			hasCreatedField: 	true,
-			hasUpdatedField: 	true,
-			createdFieldName:	'published',
-			updatedFieldName: 	'updated'
-		}
-	},
-	indexes: {
-		Org_ObjectType_Created: {
-			fields: {
-				'created':           1,
-				'object.objectType': 1,
-			}
 		},
 	},
-	events: {
-		afterInit: function (e) {
-			e.target.set("id", e.target._id);
-		},
-		beforeSave: function(e) {
-			try {
-				e.target.validate({ stopOnFirstError: false });
-			} catch (err) {
-				if (err.error === 'validation-error') {
-					err = new Meteor.Error(err.error, [].concat(err.details).map(e => e.message));
-				}
-				e.preventDefault();
-				throw err;
-			}
-		},
-		afterSave: function(e){
-			e.target.set("id", this._id);
-		}
-	}
 });
