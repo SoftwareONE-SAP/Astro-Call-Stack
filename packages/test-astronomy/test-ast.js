be302127fcf479db8d8260a9bd35974db11746e2
Jake Lacey <jakelacey2012@gmail.com>
2016-10-12 15:51:57 +0100

    Adding comments

--- packages/test-astronomy/test-ast.js
+++ packages/test-astronomy/test-ast.js
@@ -1,13 +1,17 @@
 /**
- * Activity   - Commented out no callstack error.
- * AS.Object - inherits resolvable
+ *
+ * This set of model definitions will cause this error.
+ *
+ * W20161012-15:49:43.074(1)? (STDERR) /home/jlacey/.meteor/packages/meteor-tool/.1.3.5_1.1bjykex++os.linux.x86_64+web.browser+web.cordova/mt-os.linux.x86_64/dev_bundle/server-lib/node_modules/fibers/future.js:280
+ * W20161012-15:49:43.074(1)? (STDERR) 						throw(ex);
+ * W20161012-15:49:43.074(1)? (STDERR) 						      ^
+ * W20161012-15:49:43.074(1)? (STDERR) RangeError: Maximum call stack size exceeded
+ *
  */
-
 AS = {};
 
 AS.Object = Astro.Class.create({
 	name:      'AS.Object',
-	typeField: 'objectType',
 });
 
 AS.Object.extend({
@@ -33,7 +37,10 @@ AS.Object.extend({
  */
 AS.Activity = Astro.Class.create({
 	name:      'AS.Activity',
-	typeField: 'objectType',
+  /**
+   * @NOTE
+   * Commenting the collection will stop the call stack error.
+   */
 	collection: new Meteor.Collection('as.activities'),
 	fields: {
 		fieldA: {
